6713430d91c3109f1958f988620f245fb5cce77c
Carchedi <carchedi.ufjf@gmail.com>
2024-03-06 16:29:19 -0300

    refatoracao do logo da empresa

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -6,7 +6,7 @@ function Logo() {
     <>
       <div className="d-flex justify-content-center align-items-start p-4">
         <a href="https://amicci.com.br/" target="_blank">
-          <img src="/src/images/logo.svg"  alt="Logo" className="img-fluid" style={{ width: '350px' }}/>
+          <img src="https://amicci.com.br/wp-content/uploads/2023/08/logo.svg"  alt="Logo" className="img-fluid" style={{ width: '350px' }}/>
         </a>
       </div>
     </>
