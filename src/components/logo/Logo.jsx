185ca3561cfbeedc11215fa9a0e5ff16b6a3cc4f
Carchedi <carchedi.ufjf@gmail.com>
2024-03-05 21:52:51 -0300

    Alteracao layout da aplicacao

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PersonalButton() {
+	return (
+		<>
+			<div className="">
+				<img class="mainLogo" src="/src/images/logo.svg" ></img>
+			</div>
+		</>
+	);
+}
+export default PersonalButton
---
+++ src/components/personalButton/PersonalButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+
+function PersonalButton() {
+	return (
+		<>
+			<Button variant="primary">Pesquisar</Button>
+		</>
+	);
+}
+export default PersonalButton
